29b300aab251e0a5080b5008d1ccc599e20af959
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-06 21:23:24 -0600

    Refactor: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,41 +6,41 @@ import { useAuthStore } from './store/authStore';
 import api from './services/api';
 
 function App() {
-  const { login, setInitializing, isInitializing, isAuthenticated } = useAuthStore();
+  const { login, logout, setInitializing, isInitializing, isAuthenticated } = useAuthStore();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const initializeAuth = async () => {
-      try {
-        // Petición silenciosa para obtener un nuevo accessToken
-        const response = await api.post('/auth/refresh');
-        const { accessToken, payload } = response.data;
+		const refreshToken = async () => {
+			try {
+				const response = await api('api/refresh');
+				const { accessToken } = response.data;
+				const decoded = JSON.parse(atob(accessToken.split('.')[1]));
 
-        login({ accessToken, user: payload });
-      } catch (error) {
-        // Si hay un error (ej. refresh token inválido o expirado),
-        // simplemente no hacemos login.
-        console.log('Error during silent authentication:', error);
+				login({ accessToken, user: decoded });
+			} catch (err: any) {
+				// No hay refreshToken o está vencido
+        console.error('Error refreshing token:', err?.response?.data || err.message);
         
-        console.log('No active session found.');
-      } finally {
-        // Marcamos la inicialización como completada, haya funcionado o no.
-        setInitializing(false);
-      }
-    };
+				logout();
+			} finally {
+				setInitializing(false);
+			}
+		};
 
-    if (isInitializing) {
-      initializeAuth();
-    }
-  }, [isInitializing, login, setInitializing]);
+		refreshToken();
+    
+    // if (isInitializing) {
+    //    refreshToken();
+		// 	}
+	}, []);
+
 
   useEffect(() => {
     if (!isInitializing && !isAuthenticated) {
       // Si la inicialización ha terminado y el usuario no está autenticado,
-      // lo redirigimos a la página de login.
       navigate('/login', { replace: true });
     }
-  }, [isInitializing, isAuthenticated, navigate]);
+  }, [isInitializing, isAuthenticated]);
 
   // Mientras se inicializa, puedes mostrar un spinner global
   if (isInitializing) {
