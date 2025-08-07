73105cbc9b90cebc74eb037515ca727eb1b4283c
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-06 21:32:27 -0600

    Refactor: RequireRole.tsx

--- src/components/RequireRole.tsx
+++ src/components/RequireRole.tsx
@@ -9,35 +9,29 @@ const RequireRole = ({ allowedRoles }: RequireRoleProps) => {
 	const { user, isAuthenticated, isInitializing } = useAuthStore();
 	const location = useLocation();
 
-	if (isInitializing) {
-		// Muestra un loader mientras se determina el estado de autenticación.
-		return <div>Verificando permisos...</div>;
-	}
+	// Muestra un loader mientras se determina el estado de autenticación
+	if (isInitializing) return <div>Cargando...</div>;
 
-	// El usuario está autenticado y su rol está en la lista de permitidos
-	if (isAuthenticated && user?.role && allowedRoles.includes(user.role)) {
-		return <Outlet />; // Renderiza el componente hijo (la ruta protegida)
-	}
+	// El usuario no está autenticado
+	if (!isAuthenticated || !user)
+		return (
+			<Navigate
+				to='/login'
+				replace
+				state={{ from: location }}
+			/>
+		);
 
 	// El usuario está autenticado pero no tiene el rol correcto
-	if (isAuthenticated) {
+	if (!allowedRoles.includes(user.role))
 		return (
 			<Navigate
 				to='/unauthorized'
-				state={{ from: location }}
 				replace
 			/>
 		);
-	}
 
-	// El usuario no está autenticado, App.tsx se encargará de redirigir a /login
-	return (
-		<Navigate
-			to='/login'
-			state={{ from: location }}
-			replace
-		/>
-	);
+	return <Outlet />; // Renderiza el componente hijo (la ruta protegida)
 };
 
 export default RequireRole;
