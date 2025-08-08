7dde2a86669132216438c26e7908e5ddeca12718
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-07 20:26:23 -0600

    Feat: MessageText

--- src/components/chat/ChatBubble/MessageText.tsx
+++ src/components/chat/ChatBubble/MessageText.tsx
@@ -1,5 +1,11 @@
 import { type TextMessage } from '../../../interfaces/message';
+import TimeFormat from '../TimeFormat'
 
 export default function MessageText({ message }: { message: TextMessage }) {
-	return <>{message.content}</>;
+	return (
+		<div className='pb-2.5 pt-2 pr-1.5 pl-2 -mt-2 -mb-1'>
+			<div>{message.content}</div>
+			<TimeFormat timestamp={message.timestamp} />
+		</div>
+	);
 }
