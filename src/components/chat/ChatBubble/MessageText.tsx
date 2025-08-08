0e0b581b5c4104b485c74a027aa8f02929cf5a0f
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-07 20:34:49 -0600

    Style: MessageText

--- src/components/chat/ChatBubble/MessageText.tsx
+++ src/components/chat/ChatBubble/MessageText.tsx
@@ -3,9 +3,12 @@ import TimeFormat from '../TimeFormat'
 
 export default function MessageText({ message }: { message: TextMessage }) {
 	return (
-		<div className='pb-2.5 pt-2 pr-1.5 pl-2 -mt-2 -mb-1'>
+		<div className='pb-2.5 pt-2 pr-1.5 pl-2 '>
 			<div>{message.content}</div>
-			<TimeFormat timestamp={message.timestamp} />
+
+			<div className='-mt-0.5 -mb-1'>
+				<TimeFormat timestamp={message.timestamp} />
+			</div>
 		</div>
 	);
 }
