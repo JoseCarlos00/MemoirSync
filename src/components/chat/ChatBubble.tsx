482af34caba485f51e8de34e395024ae668566d4
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-09 12:49:15 -0600

    Feat: add Reaction Emoji Text

--- src/components/chat/ChatBubble.tsx
+++ src/components/chat/ChatBubble.tsx
@@ -7,8 +7,9 @@ import MessageAudio from './ChatBubble/MessageAudio';
 import MessageVideo from './ChatBubble/MessageVideo';
 import MessageSticker from './ChatBubble/MessageSticker';
 import UnsupportedMessage from './ChatBubble/UnsupportedMessage';
-import { useUser } from '../../hooks/use.user';
 import EmojiPickerComponent from './ChatBubble/EmojiPicker';
+import { useUser } from '../../hooks/use.user';
+import api from '../../services/api'
 
 interface ChatBubbleProps {
 	message: Message;
@@ -21,6 +22,7 @@ function ChatBubble({ message, showTail = false }: ChatBubbleProps) {
 
 	const isMe = message.sender === 'me';
 	const containerClass = isMe ? 'justify-end' : 'justify-start';
+	const bubbleAlignmentClass = isMe ? 'items-end' : 'items-start';
 
 	// Clases base para la burbuja
 	const bubbleBaseClass = 'max-w-[70%] rounded-lg shadow relative';
@@ -63,29 +65,47 @@ function ChatBubble({ message, showTail = false }: ChatBubbleProps) {
 		setOpenPickerId((current) => (current === id ? null : id));
 	};
 
-	const sendReaction = (emoji: string) => {
+	const sendReaction = async (emoji: string) => {
 		console.log('Emoji enviado:', emoji);
 		setOpenPickerId(null);
+
+		try {
+			const response = api.post('/messages/reaction/', { reactionEmoji : emoji });
+
+			console.log('Respuesta del servidor:', response);
+
+		} catch (error) {
+			console.error('Error al actualizar la reacción:', error);
+		}
 	};
 
 	return (
-		<div className={`flex relative mb-2 group ${containerClass}`}>
+		<div className={`flex relative mb-1 group ${containerClass}`}>
 			{showTail && message.type !== 'sticker' && <BubbleTail isMe={isMe} />}
 
-			<div className={`${bubbleBaseClass} ${senderClass} ${tailClass} ${stickerClass} relative`}>
-				{/* Emoji Picker */}
-				{isAdmin && (
-					<EmojiPickerComponent
-						key={message._id}
-						isOpen={openPickerId === message._id}
-						onToggle={() => togglePicker(message._id)}
-						onSendReaction={sendReaction}
-						isMe={isMe}
-					/>
-				)}
+			<div className={`flex flex-col ${bubbleAlignmentClass}`}>
+				<div className={`${bubbleBaseClass} ${senderClass} ${tailClass} ${stickerClass} relative`}>
+					{/* Emoji Picker */}
+					{isAdmin && (
+						<EmojiPickerComponent
+							key={message._id}
+							isOpen={openPickerId === message._id}
+							onToggle={() => togglePicker(message._id)}
+							onSendReaction={sendReaction}
+							isMe={isMe}
+						/>
+					)}
 
-				{/* Contenido del mensaje */}
-				<div className='text-sm text-white'>{renderMessageContent()}</div>
+					{/* Contenido del mensaje */}
+					<div className='text-sm text-white relative'>{renderMessageContent()}</div>
+				</div>
+
+				{/* Emoji Reaction */}
+				{message.reactionEmoji && (
+					<div className='relative -mt-2 z-10 px-2'>
+						<span className='bg-gray-700 rounded-full px-1.5 py-1 text-xs'>{message.reactionEmoji}</span>
+					</div>
+				)}
 			</div>
 		</div>
 	);
